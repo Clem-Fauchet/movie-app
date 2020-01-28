f0d564ca7b0702ec914c3ec74e9a80c0064bcc17
Clem <clemence.fauchet23@gmail.com>
2020-01-28 21:24:42 +0100

    Fail popup - maybe error api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 
 import './App.scss'
 import Search from './components/Search'
+import Results from './components/Results'
+import Result from './components/Result'
 
 function App() {
   const [state, setState] = useState({
@@ -39,6 +41,7 @@ function App() {
       </header>
       <main>
         <Search handleInput={handleInput} search={search} />
+        <Results results={state.results} />
       </main>
     </div>
   )
