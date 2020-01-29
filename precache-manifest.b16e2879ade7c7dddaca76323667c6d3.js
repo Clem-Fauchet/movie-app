216ab911c87336bdc9ffdb9126fab9f38cf735ef
Clem <clemence.fauchet23@gmail.com>
2020-01-29 17:58:18 +0100

    Updates

---
+++ precache-manifest.b16e2879ade7c7dddaca76323667c6d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15cf214d67273c556fc77052e8692f57",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "8e746a04fa7720b642c9",
+    "url": "/movie-app/static/css/main.25611d52.chunk.css"
+  },
+  {
+    "revision": "1d59685d1666bc7f42a5",
+    "url": "/movie-app/static/js/2.a39afbe0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie-app/static/js/2.a39afbe0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e746a04fa7720b642c9",
+    "url": "/movie-app/static/js/main.4cc4429c.chunk.js"
+  },
+  {
+    "revision": "7045287b47a9eea3e63f",
+    "url": "/movie-app/static/js/runtime-main.da7e9728.js"
+  }
+]);
